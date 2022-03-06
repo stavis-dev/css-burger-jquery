ae93a3a90e21cbf88facfd1a7fb93f225e1bc6af
Stavis <stavis.vega@gmail.com>
2022-03-07 00:14:23 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// мобильное меню
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
